687429b7fed3c9267cbb9e9e4714f6e7c035c418
7700261670 <teste@21>
2020-07-29 12:14:14 -0300

    adicionado go lint no CI

--- soma_test.go
+++ soma_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T) {
+func soma(t *testing.T) {
 	result := soma(5, 5)
 	if result != 10 {
 		t.Errorf("Soma(5+5) = %v ; want 10", result)
